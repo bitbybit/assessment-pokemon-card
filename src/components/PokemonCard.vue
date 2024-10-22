a4ea89a5c2fe65c4cecd7c97739a0eeb2e1d4e4e
Raydel Abreu <raydel.abreu@tugitark.com>
2024-10-22 11:42:01 +0200

    first commit

---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="">
+    <div v-if="pokemon">
+      <img :src="pokemon.sprites.front_default" :alt="pokemon.name" class="" />
+      <h2 class="">{{ pokemon.name }}</h2>
+      <div class="">
+        <span
+          v-for="(type, index) in pokemon.types"
+          :key="index"
+          class=""
+        >
+          {{ type.type.name }}
+        </span>
+      </div>
+      <div class="my-4">
+        <h3 class="font-semibold">Base Stats:</h3>
+        <ul>
+          <li v-for="(stat, index) in pokemon.stats" :key="index">
+            {{ stat.stat.name }}: {{ stat.base_stat }}
+          </li>
+        </ul>
+      </div>
+      <button          
+        class=""        
+      >
+        Load another Pokémon
+      </button>
+    </div>
+    <div v-else>
+      <p>Loading...</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  import pokemonData from '@/stores/pokemon.json';
+  export default {
+    data() {
+      return {
+        pokemon: pokemonData,
+      };
+    },
+    methods: {      
+      
+    },
+    mounted() {
+      
+    },
+  };
+  </script>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Pokemon from '../components/PokemonCard.vue'
+</script>
+
+<template>
+  <main>
+    <Pokemon />
+  </main>
+</template>
